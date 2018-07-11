71ffbd6b14dab96b8f124b76bceaaede3fd70ef8
Dan Fox <dfox@palantir.com>
2018-07-11 23:46:04 +0100

    Run on CI

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,12 +3,12 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}" )"/..
 
 case $(uname -s) in
-    Linux*) BINARY=./target/debug/conjure-verification-server ;;
+    Linux*) BINARY=./target/release/conjure-verification-server ;;
     Darwin*) echo "unable to build linux docker image on mac" && exit 1 ;;
 esac
 
 if [ -f $BINARY ]; then
-    echo "$BINARY must exist - run 'cargo build' to create it"
+    echo "$BINARY must exist - run 'cargo build --release' to create it"
     exit 1
 fi
 
