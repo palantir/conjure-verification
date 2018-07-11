ecceb356a1930e3e7b816eff1049aaabab335cf3
Dan Fox <dfox@palantir.com>
2018-07-12 00:09:28 +0100

    Bundle test-cases.json

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,25 +3,29 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}" )"/..
 
 case $(uname -s) in
-    Linux*) BINARY=./target/release/conjure-verification-server ;;
-    Darwin*) echo "unable to build linux docker image on mac" && exit 1 ;;
+    Darwin*) echo "docker images can only be built on linux (ie on CircleCI)" && exit 1 ;;
 esac
 
+VERSION=$(git describe --tags --always --first-parent)
+DEST=build/docker-context
+rm -rf $DEST
+mkdir -p $DEST
+
+cp ./verification-server/Dockerfile $DEST/Dockerfile
+
+BINARY=./target/release/conjure-verification-server
 if [ ! -f $BINARY ]; then
     echo "$BINARY must exist - run 'cargo build --release' to create it"
     exit 1
 fi
+cp $BINARY $DEST
 
-VERSION=$(git describe --tags --always --first-parent)
-DEST=build/docker-context
-
-rm -rf $DEST
-mkdir -p $DEST
-
-cp -R $BINARY $DEST
-
-cp ./verification-server/Dockerfile $DEST/Dockerfile
+TEST_CASES=./verification-api/build/test-cases.json
+if [ ! -f $TEST_CASES ]; then
+    echo "$TEST_CASES file must exist - run './gradlew compileTestCasesJson' to create it"
+    exit 1
+fi
+cp $TEST_CASES $DEST
 
 cd $DEST
-
 docker build -t "palantirtechnologies/conjure-verification-server:$VERSION" .
