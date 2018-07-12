palantir/conjure-verification

commit dcce7a981448a905effc67e21c839e5564fcd485

Type for ignored test cases (#20)

verification-api/src/main/java/com/palantir/conjure/compliance/CompileTestCasesJson.java
@@ -17,7 +17,6 @@ import com.palantir.remoting3.ext.jackson.ObjectMappers;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -60,7 +59,7 @@ public final class CompileTestCasesJson {
                 serviceByName(ir, "SingleQueryParamService"));
     }
 
-    private static long countTestCases(Map<EndpointName, List<String>> tests) {
+    private static long countTestCases(Map<EndpointName, Set<String>> tests) {
         return tests.entrySet().stream()
                 .flatMap(e -> e.getValue().stream())
                 .count();

verification-api/src/main/conjure/test-cases.conjure.yml
@@ -10,14 +10,25 @@ types:
       ClientTestCases:
         fields:
           autoDeserialize: map<EndpointName, PositiveAndNegativeTestCases>
-          singleHeaderService: map<EndpointName, list<string>>
-          singlePathParamService: map<EndpointName, list<string>>
-          singleQueryParamService: map<EndpointName, list<string>>
+          singleHeaderService: map<EndpointName, set<string>>
+          singlePathParamService: map<EndpointName, set<string>>
+          singleQueryParamService: map<EndpointName, set<string>>
 
       EndpointName:
         alias: string
 
       PositiveAndNegativeTestCases:
         fields:
-          positive: list<string> # json strings
-          negative: list<string>
+          positive: set<string> # json strings
+          negative: set<string>
+
+      IgnoredTestCases:
+        fields:
+          client: IgnoredClientTestCases
+
+      IgnoredClientTestCases:
+        fields:
+          autoDeserialize: map<EndpointName, set<string>>
+          singleHeaderService: map<EndpointName, set<string>>
+          singlePathParamService: map<EndpointName, set<string>>
+          singleQueryParamService: map<EndpointName, set<string>>

commit 3c57d0d71d2fa03ce854319f55d5d16916827f24

[fix] test-cases.conjure.yml uses list<string> (#21)
fixes a mistake introduced in https://github.com/palantir/conjure-verification/pull/20

verification-api/src/main/java/com/palantir/conjure/compliance/CompileTestCasesJson.java
@@ -17,6 +17,7 @@ import com.palantir.remoting3.ext.jackson.ObjectMappers;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -59,7 +60,7 @@ public final class CompileTestCasesJson {
                 serviceByName(ir, "SingleQueryParamService"));
     }
 
-    private static long countTestCases(Map<EndpointName, Set<String>> tests) {
+    private static long countTestCases(Map<EndpointName, List<String>> tests) {
         return tests.entrySet().stream()
                 .flatMap(e -> e.getValue().stream())
                 .count();

verification-api/src/main/conjure/test-cases.conjure.yml
@@ -10,17 +10,17 @@ types:
       ClientTestCases:
         fields:
           autoDeserialize: map<EndpointName, PositiveAndNegativeTestCases>
-          singleHeaderService: map<EndpointName, set<string>>
-          singlePathParamService: map<EndpointName, set<string>>
-          singleQueryParamService: map<EndpointName, set<string>>
+          singleHeaderService: map<EndpointName, list<string>>
+          singlePathParamService: map<EndpointName, list<string>>
+          singleQueryParamService: map<EndpointName, list<string>>
 
       EndpointName:
         alias: string
 
       PositiveAndNegativeTestCases:
         fields:
-          positive: set<string> # json strings
-          negative: set<string>
+          positive: list<string> # json strings
+          negative: list<string>
 
       IgnoredTestCases:
         fields: