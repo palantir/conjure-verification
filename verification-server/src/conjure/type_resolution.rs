0be71e0054f7c19a68c09c4c37681232463b7139
Dan Fox <dfox@palantir.com>
2018-07-16 16:55:06 +0100

    Comment for type_resolution.rs

--- verification-server/src/conjure/type_resolution.rs
+++ verification-server/src/conjure/type_resolution.rs
@@ -11,6 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//! Converts named and anonymous Conjure Types into ResolvedTypes (which no longer contain
+//! references/aliases).
+
 use conjure::ir::*;
 use conjure::resolved_type::ResolvedType;
 
