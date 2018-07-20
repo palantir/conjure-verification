3c7835c5149cda6441ffc6ad0572c72e20c45057
Dan Sanduleac <dsanduleac@palantir.com>
2018-07-20 14:40:28 +0100

    Separate ir/resolved_type type hierarchies

--- verification-server/src/conjure/resolved_type.rs
+++ verification-server/src/conjure/resolved_type.rs
@@ -12,21 +12,62 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use conjure::ir::*;
+use conjure::ir::EnumDefinition;
+use conjure::ir::PrimitiveType;
+use conjure::ir::TypeName;
 
 /// Similar to the conjure::ir::Type, but doesn't have a `Reference` variant.
 /// Instead, these are inlined.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum ResolvedType {
     // named types
-    Object(ObjectDefinition<ResolvedType>),
+    Object(ObjectDefinition),
     Enum(EnumDefinition),
-    Union(UnionDefinition<ResolvedType>),
+    Union(UnionDefinition),
 
     // anonymous types
     Primitive(PrimitiveType),
-    Optional(OptionalType<ResolvedType>),
-    List(ListType<ResolvedType>),
-    Set(SetType<ResolvedType>),
-    Map(MapType<PrimitiveType, ResolvedType>),
+    Optional(OptionalType),
+    List(ListType),
+    Set(SetType),
+    Map(MapType),
+}
+
+#[derive(Debug, Clone)]
+pub struct ListType {
+    pub item_type: Box<ResolvedType>,
+}
+
+#[derive(Debug, Clone)]
+pub struct SetType {
+    pub item_type: Box<ResolvedType>,
+}
+
+#[derive(Debug, Clone)]
+pub struct MapType {
+    pub key_type: Box<PrimitiveType>,
+    pub value_type: Box<ResolvedType>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FieldDefinition {
+    pub field_name: String,
+    pub type_: ResolvedType,
+}
+
+#[derive(Debug, Clone)]
+pub struct OptionalType {
+    pub item_type: Box<ResolvedType>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ObjectDefinition {
+    pub type_name: TypeName,
+    pub fields: Vec<FieldDefinition>,
+}
+
+#[derive(Debug, Clone)]
+pub struct UnionDefinition {
+    pub type_name: TypeName,
+    pub union: Vec<FieldDefinition>,
 }
