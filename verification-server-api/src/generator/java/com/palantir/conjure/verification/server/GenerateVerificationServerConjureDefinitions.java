0e449445bb8b1e18f94b3af37679ca1470b280be
Dan Sanduleac <dsanduleac@palantir.com>
2018-11-19 21:24:21 +0000

    verification-server: generate conjure and test-cases.yml

---
+++ verification-server-api/src/generator/java/com/palantir/conjure/verification/server/GenerateVerificationServerConjureDefinitions.java
@@ -0,0 +1,176 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.verification.server;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.io.MoreFiles;
+import com.google.common.io.RecursiveDeleteOption;
+import com.palantir.conjure.verification.AllTestCases;
+import com.palantir.conjure.verification.BodyTests;
+import com.palantir.conjure.verification.ResolveLocalReferencesConjureTypeVisitor;
+import com.palantir.conjure.verification.SingleHeaderParamTests;
+import com.palantir.conjure.verification.SinglePathParamTests;
+import com.palantir.conjure.verification.SingleQueryParamTests;
+import com.palantir.conjure.verification.TestCasesUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public final class GenerateVerificationServerConjureDefinitions {
+
+    public static void main(String... args) throws IOException {
+        Preconditions.checkArgument(args.length == 2, "Usage: <test-cases.yml> <conjure dir>");
+        File file = new File(args[0]);
+        File conjureDir = new File(args[1]);
+        File outputDir = new File(conjureDir, "generated");
+
+        AllTestCases testCases = TestCasesUtils.parseTestCases(file);
+
+        // Delete old contents
+        MoreFiles.deleteDirectoryContents(outputDir.toPath(), RecursiveDeleteOption.ALLOW_INSECURE);
+
+        TestCasesUtils.YAML_MAPPER.writeValue(new File(outputDir, "services.conjure.yml"),
+                createConjureYmlBuilder()
+                        .put("services", ImmutableMap.of(
+                                "AutoDeserializeService",
+                                        generateAutoDeserializeService(testCases.getBody()),
+                                "SingleHeaderService",
+                                        generateSingleHeaderService(testCases.getSingleHeaderParam()),
+                                "SinglePathParamService",
+                                        generateSinglePathParamService(testCases.getSinglePathParam()),
+                                "SingleQueryParamService",
+                                        generateSingleQueryParamService(testCases.getSingleQueryParam())
+                        ))
+                        .build());
+    }
+
+    private static Builder<String, Object> createConjureYmlBuilder() {
+        Builder<String, Object> builder = ImmutableMap.builder();
+        builder.put(
+                "types",
+                ImmutableMap.of("conjure-imports",
+                        ImmutableMap.of(
+                                "examples", "../example-types.conjure.yml",
+                                "testCases", "../test-cases.conjure.yml")));
+        return builder;
+    }
+
+    private static Map<String, Object> generateAutoDeserializeService(List<BodyTests> bodyTests) {
+        Builder<String, Object> endpoints = ImmutableMap.builder();
+
+        bodyTests.stream().map(BodyTests::getType).map(TestCasesUtils::parseConjureType).forEach(conjureType -> {
+            String endpointName = ServerTestCasesUtils.typeToEndpointName(conjureType);
+            String typeName = conjureType.visit(new ResolveLocalReferencesConjureTypeVisitor());
+            endpoints.put(endpointName, ImmutableMap.of(
+                    "http", "GET /" + endpointName + "/{index}",
+                    "returns", typeName,
+                    "args", ImmutableMap.of("index", "integer")));
+        });
+
+        return ImmutableMap.of(
+                "name", "Auto Deserialize Service",
+                "package", "com.palantir.conjure.verification.server",
+                "default-auth", "none",
+                "base-path", "/body",
+                "endpoints", endpoints.build());
+    }
+
+    private static Map<String, Object> generateSingleHeaderService(List<SingleHeaderParamTests> testCases) {
+        Builder<String, Object> endpoints = ImmutableMap.builder();
+
+        testCases
+                .stream()
+                .map(SingleHeaderParamTests::getType)
+                .map(TestCasesUtils::parseConjureType)
+                .forEach(conjureType -> {
+                    String endpointName = ServerTestCasesUtils.typeToEndpointName(conjureType);
+                    String typeName = conjureType.visit(new ResolveLocalReferencesConjureTypeVisitor());
+                    endpoints.put(endpointName, ImmutableMap.of(
+                            "http", "POST /" + endpointName + "/{index}",
+                            "args", ImmutableMap.of(
+                                    "index", "integer",
+                                    "header", ImmutableMap.of(
+                                            "type", typeName,
+                                            "param-type", "header",
+                                            "param-id", "Some-Header"))));
+                });
+
+        return ImmutableMap.of(
+                "name", "Single Header Service",
+                "package", "com.palantir.conjure.verification.server",
+                "default-auth", "none",
+                "base-path", "/single-header-param",
+                "endpoints", endpoints.build());
+    }
+
+    private static Map<String, Object> generateSinglePathParamService(List<SinglePathParamTests> testCases) {
+        Builder<String, Object> endpoints = ImmutableMap.builder();
+
+        testCases
+                .stream()
+                .map(SinglePathParamTests::getType)
+                .map(TestCasesUtils::parseConjureType)
+                .forEach(conjureType -> {
+                    String endpointName = ServerTestCasesUtils.typeToEndpointName(conjureType);
+                    String typeName = conjureType.visit(new ResolveLocalReferencesConjureTypeVisitor());
+                    endpoints.put(endpointName, ImmutableMap.of(
+                            "http", "POST /" + endpointName + "/{index}/{param}",
+                            "args", ImmutableMap.of(
+                                    "index", "integer",
+                                    "param", typeName)));
+                });
+
+        return ImmutableMap.of(
+                "name", "Single Path Param Service",
+                "package", "com.palantir.conjure.verification.server",
+                "default-auth", "none",
+                "base-path", "/single-path-param",
+                "endpoints", endpoints.build());
+    }
+
+    private static Map<String, Object> generateSingleQueryParamService(List<SingleQueryParamTests> testCases) {
+        Builder<String, Object> endpoints = ImmutableMap.builder();
+
+        testCases
+                .stream()
+                .map(SingleQueryParamTests::getType)
+                .map(TestCasesUtils::parseConjureType)
+                .forEach(conjureType -> {
+                    String endpointName = ServerTestCasesUtils.typeToEndpointName(conjureType);
+                    String typeName = conjureType.visit(new ResolveLocalReferencesConjureTypeVisitor());
+                    endpoints.put(endpointName, ImmutableMap.of(
+                            "http", "POST /" + endpointName + "/{index}",
+                            "args", ImmutableMap.of(
+                                    "index", "integer",
+                                    "someQuery", ImmutableMap.of(
+                                            "type", typeName,
+                                            "param-type", "query",
+                                            "param-id", "foo"))));
+                });
+
+        return ImmutableMap.of(
+                "name", "Single Query Param Service",
+                "package", "com.palantir.conjure.verification.server",
+                "default-auth", "none",
+                "base-path", "/single-query-param",
+                "endpoints", endpoints.build());
+    }
+
+}
---
+++ verification-server-api/src/main/java/com/palantir/conjure/verification/server/GenerateVerificationServerTestCases.java
@@ -0,0 +1,99 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.verification.server;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.palantir.conjure.verification.AllTestCases;
+import com.palantir.conjure.verification.BodyTests;
+import com.palantir.conjure.verification.ConjureTypeRepr;
+import com.palantir.conjure.verification.SingleHeaderParamTests;
+import com.palantir.conjure.verification.SinglePathParamTests;
+import com.palantir.conjure.verification.SingleQueryParamTests;
+import com.palantir.conjure.verification.TestCase;
+import com.palantir.conjure.verification.TestCasesUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public final class GenerateVerificationServerTestCases {
+    public static void main(String... args) throws IOException {
+        Preconditions.checkArgument(args.length == 2, "Usage: <test-cases.yml> <server-test-cases.yml>");
+        File file = new File(args[0]);
+        File outputFile = new File(args[1]);
+
+        AllTestCases allTestCases = TestCasesUtils.parseTestCases(file);
+
+        TestCases testCases = TestCases.of(ClientTestCases.builder()
+                .autoDeserialize(generateBodyTestCases(allTestCases.getBody()))
+                .singleHeaderService(generateSingleHeaderParamTestCases(allTestCases.getSingleHeaderParam()))
+                .singleQueryParamService(generateSingleQueryParamTestCases(allTestCases.getSingleQueryParam()))
+                .singlePathParamService(generateSinglePathParamTestCases(allTestCases.getSinglePathParam()))
+                .build());
+
+        TestCasesUtils.YAML_MAPPER.writeValue(outputFile, testCases);
+    }
+
+    private static Map<EndpointName, PositiveAndNegativeTestCases> generateBodyTestCases(List<BodyTests> bodyTests) {
+        Builder<EndpointName, PositiveAndNegativeTestCases> builder = ImmutableMap.builder();
+        bodyTests.forEach(t ->
+                builder.put(endpointName(t.getType()),
+                PositiveAndNegativeTestCases
+                        .builder()
+                        .positive(t.getBothPositive().stream().map(TestCase::get).collect(Collectors.toList()))
+                        .addAllPositive(t
+                                .getClientPositiveServerFail()
+                                .stream()
+                                .map(TestCase::get)
+                                .collect(Collectors.toList()))
+                        .negative(t.getBothNegative().stream().map(TestCase::get).collect(Collectors.toList()))
+                        .build()));
+        return builder.build();
+    }
+
+    private static Map<EndpointName, List<String>> generateSingleHeaderParamTestCases(List<SingleHeaderParamTests> singleHeaderParam) {
+        Builder<EndpointName, List<String>> builder = ImmutableMap.builder();
+        singleHeaderParam.forEach(t -> builder.put(
+                endpointName(t.getType()),
+                t.getBothPositive().stream().map(TestCase::get).collect(Collectors.toList())));
+        return builder.build();
+    }
+
+    private static Map<EndpointName, List<String>> generateSingleQueryParamTestCases(List<SingleQueryParamTests> singleQueryParam) {
+        Builder<EndpointName, List<String>> builder = ImmutableMap.builder();
+        singleQueryParam.forEach(t -> builder.put(
+                endpointName(t.getType()),
+                t.getBothPositive().stream().map(TestCase::get).collect(Collectors.toList())));
+        return builder.build();
+    }
+
+    private static Map<EndpointName, List<String>> generateSinglePathParamTestCases(List<SinglePathParamTests> singlePathParam) {
+        Builder<EndpointName, List<String>> builder = ImmutableMap.builder();
+        singlePathParam.forEach(t -> builder.put(
+                endpointName(t.getType()),
+                t.getBothPositive().stream().map(TestCase::get).collect(Collectors.toList())));
+        return builder.build();
+    }
+
+    private static EndpointName endpointName(ConjureTypeRepr type) {
+        return EndpointName.of(
+                ServerTestCasesUtils.typeToEndpointName(TestCasesUtils.parseConjureType(type)));
+    }
+}
